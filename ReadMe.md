12130a8dc5d1b613d9ef4f750c7e9d119a148130
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2023-03-11 16:24:54 +0100

    Added awesomeness

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,47 @@
 # SentimentalAnalysisBySalim
+
+## Getting Started
+
+### Testing Flask App locally
+
+Using the terminal;
+
+Clone the repository
+
+```
+git clone https://github.com/salimcodes/translatedbysalim.git
+cd translatedbysalim
+```
+
+
+Thereafter, create a virtual environment named `mypython`.
+
+```
+# Windows
+python -m venv mypython
+
+# macOS or Linux
+python -m venv mypython
+```
+
+Activate the created virtual environment
+```
+# Windows
+mypython\Scripts\activate
+
+#macOS or Linux
+source ./mypython/bin/activate
+```
+
+Then install the necessary dependencies needed.
+
+``` 
+pip install -r requirements.txt
+```
+
+Run the flask app locally
+
+```
+flask run
+```
+
--- app.py
+++ app.py
@@ -1,15 +1,70 @@
 # This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
-import os
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from dotenv import load_dotenv
 load_dotenv()
+import requests, os, uuid, json
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
 
 language_key = os.getenv("LANGUAGE_KEY")
 language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
-
-# Authenticate the client using your key and endpoint 
 def authenticate_client():
+    ta_credential = AzureKeyCredential(language_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=language_endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+    # Read the values from the form
+    original_text = request.form['text']
+    documents = [original_text]
+#def sentiment_analysis_with_opinion_mining_example(client):
+
+    #documents = [
+    #    "Manchester United is the best club in the world"
+    #]
+
+    #documents = input("Enter your review: ")
+    #documents = [documents]
+
+    result = client.analyze_sentiment(documents, show_opinion_mining=True)
+    doc_result = [doc for doc in result if not doc.is_error]
+
+    positive_reviews = [doc for doc in doc_result if doc.sentiment == "positive"]
+    negative_reviews = [doc for doc in doc_result if doc.sentiment == "negative"]
+
+    positive_mined_opinions = []
+    mixed_mined_opinions = []
+    negative_mined_opinions = []
+
+    for document in doc_result:
+        sentiment = document.sentiment
+        positive_confidence_scores = document.confidence_scores.positive
+        negative_confidence_scores = document.confidence_scores.negative
+        neutral_confidence_scores = document.confidence_scores.neutral
+    return render_template(
+        'results.html', 
+        sentiment = sentiment, 
+        positive_confidence_scores = positive_confidence_scores,
+        negative_confidence_scores = negative_confidence_scores,
+        neutral_confidence_scores = neutral_confidence_scores
+        )
+
+
+
+
+"""def authenticate_client():
     ta_credential = AzureKeyCredential(language_key)
     text_analytics_client = TextAnalyticsClient(
             endpoint=language_endpoint, 
@@ -45,4 +100,4 @@ def sentiment_analysis_with_opinion_mining_example(client):
             document.confidence_scores.neutral,
             document.confidence_scores.negative,
         ))
-sentiment_analysis_with_opinion_mining_example(client)
+sentiment_analysis_with_opinion_mining_example(client)"""
