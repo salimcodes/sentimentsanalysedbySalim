2f08c7aea1cb60001cd57375ee175e972b738754
Salim Ọlánrewájú Oyinlọlá <salimoyinlola@gmail.com>
2023-03-11 13:12:05 +0100

    Sentimental Analysis Logic added

---
+++ app.py
@@ -0,0 +1,68 @@
+# This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+#import os
+#language_key = os.environ.get('LANGUAGE_KEY')
+#language_endpoint = os.environ.get('LANGUAGE_ENDPOINT')
+language_key = "5c2af3356ca74a55971ee3e2f070a73d"
+language_endpoint = "https://sentimentalanalysisbysalim.cognitiveservices.azure.com"
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+# Authenticate the client using your key and endpoint 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(language_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=language_endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+# Example method for detecting sentiment and opinions in text 
+def sentiment_analysis_with_opinion_mining_example(client):
+
+    documents = [
+        "The food and service were unacceptable. The concierge was nice, however."
+    ]
+
+    result = client.analyze_sentiment(documents, show_opinion_mining=True)
+    doc_result = [doc for doc in result if not doc.is_error]
+
+    positive_reviews = [doc for doc in doc_result if doc.sentiment == "positive"]
+    negative_reviews = [doc for doc in doc_result if doc.sentiment == "negative"]
+
+    positive_mined_opinions = []
+    mixed_mined_opinions = []
+    negative_mined_opinions = []
+
+    for document in doc_result:
+        print("Document Sentiment: {}".format(document.sentiment))
+        print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+            document.confidence_scores.positive,
+            document.confidence_scores.neutral,
+            document.confidence_scores.negative,
+        ))
+        for sentence in document.sentences:
+            print("Sentence: {}".format(sentence.text))
+            print("Sentence sentiment: {}".format(sentence.sentiment))
+            print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+                sentence.confidence_scores.positive,
+                sentence.confidence_scores.neutral,
+                sentence.confidence_scores.negative,
+            ))
+            for mined_opinion in sentence.mined_opinions:
+                target = mined_opinion.target
+                print("......'{}' target '{}'".format(target.sentiment, target.text))
+                print("......Target score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
+                    target.confidence_scores.positive,
+                    target.confidence_scores.negative,
+                ))
+                for assessment in mined_opinion.assessments:
+                    print("......'{}' assessment '{}'".format(assessment.sentiment, assessment.text))
+                    print("......Assessment score:\n......Positive={0:.2f}\n......Negative={1:.2f}\n".format(
+                        assessment.confidence_scores.positive,
+                        assessment.confidence_scores.negative,
+                    ))
+            print("\n")
+        print("\n")
+          
+sentiment_analysis_with_opinion_mining_example(client)
